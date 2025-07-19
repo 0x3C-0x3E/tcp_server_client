693c82a6d80cbba94ab93d955550d408fb95565e
0x3C-0x3E <rbng0094@gmail.com>
2025-07-19 15:36:39 +0200

    implemented abstraction, gotta make a server and client now..

---
+++ abstraction/src/threads.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "libs/tinycsocket.h"
+#include "packets/packets.h"
+#include "libs/cthreads.h"
+
+typedef struct {
+    bool new_send_data; 
+    struct cthreads_mutex send_lock;
+    uint8_t* send_buffer;
+    size_t send_buffer_size;
+} SendData;
+
+typedef struct {
+    TcsSocket socket;
+    void (*packet_handle_function) (uint8_t* payload, size_t payload_size);
+
+    SendData send_data;
+} ThreadData;
+
+typedef struct {
+    struct cthreads_thread recv_thread;
+    struct cthreads_thread send_thread;
+
+    ThreadData thread_data;
+
+} ThreadCollection;
+
+void threads_init(ThreadCollection* collection, void (*packet_handle_function) (uint8_t* payload, size_t payload_size), TcsSocket socket);
+
+void* threads_run_recv_thread(void* data); // ThreadCollection collection
+
+void* threads_run_send_thread(void* data); // ThreadCollection collection
+
+void threads_recv_into_buffer(ThreadCollection* collection, uint8_t* buffer, size_t size);
+
+void threads_queue_new_send_data(ThreadCollection* collection, uint8_t* buffer, size_t buffer_size);
+
+void threads_kill_threads(ThreadCollection* collection);
